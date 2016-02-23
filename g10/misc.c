62254fc2ef643a0fee7d9aa78090093eff9937ef
nikitasius <admin@12pings.net>
2016-02-23 21:30:39 +0100

    normally to pass with 32768 key we use 33792=32768+1024 from while code with +1024 each step.

--- g10/misc.c
+++ g10/misc.c
@@ -602,7 +602,7 @@ pct_expando(const char *string,struct expando_args *args)
       if(!done)
 	{
 	  /* 8192 is way bigger than we'll need here */
-	  if(maxlen>=8192)
+	  if(maxlen>=33792)
 	    goto fail;
 
 	  maxlen+=1024;
